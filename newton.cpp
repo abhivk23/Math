c492657d5dd424fb6c86270fe859530e82158d7d
abhivk23 <abhivk23@gmail.com>
2020-08-19 00:16:29 -0500

    newton solver for sqrt

---
+++ newton.cpp
@@ -0,0 +1,22 @@
+// Abhi Kauturu
+// A square root solver based on Newton's Method of root-finding
+
+#include <iostream>
+#include <vector>
+
+float square_root(float A, float x0, float x1){
+    std::vector<float> x{x0, x1};
+    
+    int j=1;
+    while((x[j-1]-x[j]) > 0.000001){
+        x[j+1] = 0.5*(x[j]+A/x[j]);
+        j++;
+    }
+
+    return x[j];
+}
+
+int main() {
+    std::cout << square_root(42,7.0,6.5) << "\n";
+    return 0;
+}
